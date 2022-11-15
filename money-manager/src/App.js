11fe2285bf3abdeea15cfd7ac5e7b7c08b40ac3f
Anuja mukkawar <anujamukkawar21@gmail.com>
2022-11-15 20:50:24 +0530

    new change

--- money-manager/src/App.js
+++ money-manager/src/App.js
@@ -1,83 +1,12 @@
 import './App.css';
-import { Navigate, Routes, Route, Link, useNavigate } from "react-router-dom";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import Paper from '@mui/material/Paper';
-import { Overview } from './Overview';
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import { Balance } from './components/Balance.js'
 
-const drawerWidth = 240;
-
-
-function App() {
-  const navigate = useNavigate();
+export default function App() {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-         sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-      >
-        <Toolbar>
-        
-        <Typography variant="h6" noWrap component="div">
-          Permanent drawer
-        </Typography>
-      </Toolbar>
-      </AppBar>
-      
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="left">
-         
-        <Toolbar />
-        <h3>MONEY MANAGER</h3>
-        <Divider />
-        <List>
-          {['Overview', 'Lifeplans', 'Finance Tools', 'Finance Coach', 'Settings'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-      </Drawer>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}
-      >
-        <Toolbar />
-
-      </Box>
-    </Box>
+    <div>
+      <h1>Expence Tracker</h1>
+      <Balance/>
+    </div>
   );
 }
-
-export default App;

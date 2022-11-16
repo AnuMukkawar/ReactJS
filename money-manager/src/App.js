be68b029e1b1aca2c232c9a37b916e607ce671ca
Anuja mukkawar <anujamukkawar21@gmail.com>
2022-11-16 19:44:12 +0530

    addin components

--- money-manager/src/App.js
+++ money-manager/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import * as React from 'react';
 import { Balance } from './components/Balance.js'
+import { IncomeExpense } from './components/IncomeExpense';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 export default function App() {
   return (
     <div>
-      <h1>Expence Tracker</h1>
+      <h1>Expence Tracker</h1><br/>
       <Balance/>
+      <IncomeExpense/>
+      <TransactionList/>
+      <AddTransaction/>
     </div>
   );
 }

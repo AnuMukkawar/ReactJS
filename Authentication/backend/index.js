0ec9f77fd709dc8e532164da102e6042db1a4f06
Anumukkawar <anujamukkawar21@gmail.com>
2022-12-22 16:28:42 +0530

    login functionality

---
+++ Authentication/backend/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import {MongoClient} from 'mongodb';
+import dotenv from 'dotenv'
+import cors from 'cors';
+
+dotenv.config()
+
+const app=express();
+app.use(cors());
+
+const port=process.env.PORT;
+const MONGO_URL=process.env.MONGO_URL;
+// const MONGO_URL="mongodb://localhost";
+
+async function createConnection(){
+    const client=new MongoClient(MONGO_URL);
+    await client.connect();
+    return client;
+}
+
+export const client =await createConnection();
+app.use(express.json());
+// app.get("/",(req,res)=>{
+//     res.send("Hello");
+// });
+app.use("/api/users")
+
+app.listen(port,()=>console.log("Server is started ",port));

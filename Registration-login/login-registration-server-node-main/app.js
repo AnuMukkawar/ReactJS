69da8541453bebdcdd83122a7ac4ae42312bfc1d
Anumukkawar <anujamukkawar21@gmail.com>
2022-12-22 18:53:47 +0530

    change

--- Registration-login/login-registration-server-node-main/app.js
+++ Registration-login/login-registration-server-node-main/app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 const jwt = require("jsonwebtoken");
 var nodemailer = require("nodemailer");
 
-const JWT_SECRET =SECREATKEY;
+const JWT_SECRET ="hvdvay6ert72839289()aiyg8t87qt72393293883uhefiuh78ttq3ifi78272jbkj?[]]pou89ywe";
 
 const mongoUrl = MONGO_URL;
 

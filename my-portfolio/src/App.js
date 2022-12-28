0b84afcdd4c379d129aa3c782e9035fb307c1e0d
Anumukkawar <anujamukkawar21@gmail.com>
2022-12-28 21:09:21 +0530

    about page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 // import AppBar from '@mui/material/AppBar';
 // import Toolbar from '@mui/material/Toolbar';
 // import Button from '@mui/material/Button';
-import {Header}  from './components/header/Header';
-import {Home} from './components/home/Home';
-// import { About } from './components/About';
+import {Header}  from './components/header/Header.js';
+import {Home} from './components/home/Home.js';
+import {About} from './components/about/About.js';
 // import { BrowserRouter as Router, Switch, Route,Routes,Link } from "react-router-dom"
 
 
@@ -15,6 +15,7 @@ function App() {
     <Header />
     <main className='main'>
       <Home/>
+      <About/>
     </main>
     {/* <Routes>
       <Route path='/' element={<Home/>}/>
---
+++ my-portfolio/src/components/about/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./About.css";
+import AboutImg from "./profile.jpg";
+import Resume from "./AnujaM-Resume.pdf";
+import {Info} from "./Info";
+
+const About=()=>{
+    return(
+        <section className="about section" id="about">
+            <h2 className="section__title">About Me</h2>
+            <span className="section__subtitle">My introduction</span>
+            <div className="about__container container grid">
+                <img src= {AboutImg} alt="" className="about___img"></img>
+                <div className="about__data">
+                    <Info/>
+
+                    <p className="about__description">
+                     I completed my graduation in computer science and enginerring in 2019. 
+                     I am Full stack developer, I create web pages with UI user interface.
+                     Currently,I am working as SKills Developer in Aera Technology, Pune. I have 3 years of experience in SQL.
+                     Now, I want to explore new opportunities in full stack development.
+                    </p>
+
+                    <a download="" href={Resume} className="button button--flex"> Download CV</a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export {About};

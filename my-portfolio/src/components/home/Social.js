7b0b32281aa4cc457f75c10b7ffa746f588129b8
Anumukkawar <anujamukkawar21@gmail.com>
2022-12-31 19:36:37 +0530

    changes

--- my-portfolio/src/components/home/Social.js
+++ my-portfolio/src/components/home/Social.js
@@ -3,10 +3,10 @@ import React from "react";
  const Social=()=>{
     return(
         <div className="home__social">
-            <a href="https://github.com/" className="home__social-icon" target="_blank">
+            <a href="https://github.com/" className="home__social-icon" target="_blank" rel="noreferrer" >
             <i class="uil uil-github"></i>
             </a>
-            <a href="https://www.linkedin.coc/in/anuja-mukkawar-663661170"  className="home__social-icon" target="_blank">
+            <a href="https://www.linkedin.coc/in/anuja-mukkawar-663661170"  className="home__social-icon" target="_blank" rel="noreferrer" >
             <i class="uil uil-linkedin"></i>
             </a>
         

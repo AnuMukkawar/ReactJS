704a24c3af2d82dc3fe37ea251068091a4bd02c1
Anumukkawar <anujamukkawar21@gmail.com>
2023-01-31 19:40:58 +0530

    added two more projects

--- my-portfolio/src/components/projects/Projects.js
+++ my-portfolio/src/components/projects/Projects.js
@@ -52,9 +52,9 @@ const Projects=()=>{
                             <i onClick={()=>toggleTab(0)} className="uil uil-times projects__modal-close"></i>
 
                             <h3 className="projetcs__modal-title">Money Manager App</h3>
-                            {/* <p className="projects__modal-decription">
-                                I built this app using React for frontend and Node Js for backend.
-                            </p> */}
+                             <p className="projects__modal-decription">
+                                 Tech stack used- HTML,CSS,Javascript,ReactJS,NodeJS,MongoDB
+                            </p> 
                             <ul className="projects__modal-projects grid">
                                 <li className="projects__modal-projects">
                                     <i className="uil uil-check-circle projects__modal-icon"></i>
@@ -79,7 +79,7 @@ const Projects=()=>{
                         <i class="uil uil-film projects__icon"></i>
                         <h3 className="projects__title">Movie Flix App</h3>
                     </div>
-                    <span className="projects__button" onClick={()=>toggleTab(3)}>View More <i className="uil uil-arrow-right projects__button-icon"></i></span>
+                    <span className="projects__button" onClick={()=>toggleTab(3)}>source code <i className="uil uil-arrow-right projects__button-icon"></i></span>
 
                     <div className={toggelState===3 ? "projects__modal active-modal" : "projects__modal"}>
                         <div className="projects__modal-content">
@@ -88,25 +88,124 @@ const Projects=()=>{
                             <h3 className="projetcs__modal-title">Movie Flix App</h3>
                             <p className="projects__modal-decription">
                                 This app is for showing the movies list. We can add/delete movies.
+                                 Tech stack used- HTML,CSS,Javascript,ReactJS,NodeJS,MongoDB
                             </p>
                             <ul className="projects__modal-projects grid">
-                                
+                            
                                 <li className="projects__modal-projects">
                                     <i className="uil uil-check-circle projects__modal-icon"></i>
                                     <p className="projects__modal-info">
-                                        Frontend source code- ReactJS/tree/master/movie-app/
+                                        Frontend- ReactJS/tree/master/movie-app/
                                     </p>
                                 </li>
                                 <li className="projects__modal-projects">
                                     <i className="uil uil-check-circle projects__modal-icon"></i>
                                     <p className="projects__modal-info">
-                                        Backend source code- Backend-MovieApp/
+                                        Backend- Backend-MovieApp/
                                     </p>
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </div>
+
+                <div className="projects__content">
+                    <div>
+                        <i class="uil uil-book projects__icon"></i> 
+                        <h3 className="projects__title"> Hotel Booking Api</h3>
+                    </div>
+                    <span className="projects__button" onClick={()=>toggleTab(5)}>URL<i className="uil uil-arrow-right projects__button-icon"></i></span>
+                    <span className="projects__button" onClick={()=>toggleTab(6)}>Source code <i className="uil uil-arrow-right projects__button-icon"></i></span>
+
+                    <div className={toggelState===5 ? "projects__modal active-modal" : "projects__modal"}>
+                        <div className="projects__modal-content">
+                            <i onClick={()=>toggleTab(0)} className="uil uil-times projects__modal-close"></i>
+
+                            <h3 className="projetcs__modal-title"> Hotel Booking Api</h3>
+                            <p className="projects__modal-decription">
+                                This app is having functionality to book a room and it will show how many rooms are booked and which are vacant.
+                            </p>
+                            <ul className="projects__modal-projects grid">
+                                <li className="projects__modal-projects">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
+                                    Backend- https://booking-api-sch3.onrender.com
+                                    </p>
+                                </li>
+                            
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className={toggelState===6 ? "projects__modal active-modal" : "projects__modal"}>
+                        <div className="projects__modal-content">
+                            <i onClick={()=>toggleTab(0)} className="uil uil-times projects__modal-close"></i>
+
+                            <h3 className="projetcs__modal-title">Hotel Booking Api</h3>
+                            <p className="projects__modal-decription">
+                                 Tech stack used- HTML,CSS,Javascript,NodeJS
+                            </p> 
+                            <ul className="projects__modal-projects grid">
+                                <li className="projects__modal-projects">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
+                                        Backend git repo- Booking-Api/
+                                    </p>
+                                </li>
+                            
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="projects__content">
+                    <div>
+                        <i class="uil uil-calculator projects__icon"></i> 
+                        <h3 className="projects__title"> Calculator App</h3>
+                    </div>
+                    <span className="projects__button" onClick={()=>toggleTab(7)}>URL<i className="uil uil-arrow-right projects__button-icon"></i></span>
+                    <span className="projects__button" onClick={()=>toggleTab(8)}>Source code <i className="uil uil-arrow-right projects__button-icon"></i></span>
+
+                    <div className={toggelState===7 ? "projects__modal active-modal" : "projects__modal"}>
+                        <div className="projects__modal-content">
+                            <i onClick={()=>toggleTab(0)} className="uil uil-times projects__modal-close"></i>
+
+                            <h3 className="projetcs__modal-title">Calculator App</h3>
+                            
+                            <ul className="projects__modal-projects grid">
+                                <li className="projects__modal-projects">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
+                                        Frontend- https://heartfelt-sunflower-57bf92.netlify.app/
+                                    </p>
+                                </li>
+                            
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className={toggelState===8 ? "projects__modal active-modal" : "projects__modal"}>
+                        <div className="projects__modal-content">
+                            <i onClick={()=>toggleTab(0)} className="uil uil-times projects__modal-close"></i>
+
+                            <h3 className="projetcs__modal-title">Calculator App</h3>
+                             <p className="projects__modal-decription">
+                                 Tech stack used- HTML,CSS,Javascript
+                            </p> 
+                            <ul className="projects__modal-projects grid">
+                                <li className="projects__modal-projects">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
+                                        Frontend git repo- HTML/Day16_Tasks/CalculatorTask
+                                    </p>
+                                </li>
+                            
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         </section>       
     )
